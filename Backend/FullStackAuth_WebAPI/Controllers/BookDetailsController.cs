f8b36528701826140abc260c0fe5e2abf7415f0c
LizzieDworsky <amydworsky15@gmail.com>
2023-07-12 15:11:22 -0600

    added BookDetailsController

---
+++ Backend/FullStackAuth_WebAPI/Controllers/BookDetailsController.cs
@@ -0,0 +1,26 @@
+﻿using FullStackAuth_WebAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FullStackAuth_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookDetailsController : ControllerBase
+    {
+        public readonly ApplicationDbContext _context;
+
+        public BookDetailsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+    }
+}
